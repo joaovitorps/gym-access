6e4c7989f986bc5c76a926648d392e8e4dbbf6de
Joao Santos <dev.joaosantos@outlook.com>
2026-03-26 20:16:10 -0400

    change biome `indentStyle` to tab and apply format

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,19 +2,19 @@ import "dotenv/config";
 import { z } from "zod";
 
 const EnvSchema = z.object({
-	NODE_ENV: z.enum(["dev", "test", "production"]),
-	PORT: z.coerce.number().default(3333),
-	DATABASE_URL: z.string(),
+  NODE_ENV: z.enum(["dev", "test", "production"]),
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string(),
 });
 
 const _env = EnvSchema.safeParse(process.env);
 
 if (!_env.success) {
-	console.error(
-		"❌Environment variables incorrectly set!",
-		z.treeifyError(_env.error),
-	);
-	throw new Error("Environment variables incorrectly set!");
+  console.error(
+    "❌Environment variables incorrectly set!",
+    z.treeifyError(_env.error),
+  );
+  throw new Error("Environment variables incorrectly set!");
 }
 
 export const env = _env.data;
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,9 @@ import { app } from "./app.js";
 import { env } from "./env/index.js";
 
 app.listen(
-	{
-		host: "0.0.0.0",
-		port: env.PORT,
-	},
-	(_err, address) => console.log(`🚀 Server started at ${address}!`),
+  {
+    host: "0.0.0.0",
+    port: env.PORT,
+  },
+  (_err, address) => console.log(`🚀 Server started at ${address}!`),
 );
