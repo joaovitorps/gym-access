a47cb24e6122d37c18998471a23c9c3b3983f573
João Vítor P. Santos <48125062+joaovitorps@users.noreply.github.com>
2026-07-14 10:51:34 -0300

    feat(app): add CORS support and WEB_URL env variable

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import cookie from "@fastify/cookie";
+import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
 import { fastify } from "fastify";
 import * as z from "zod";
@@ -7,7 +8,15 @@ import { checkInRoutes } from "./http/controllers/check-ins/routes";
 import { gymRoutes } from "./http/controllers/gyms/routes";
 import { userRoutes } from "./http/controllers/users/routes";
 
-export const app = fastify();
+export const app = fastify({
+  logger: true,
+});
+
+app.register(cors, {
+  origin: env.WEB_URL,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+});
 
 app.register(jwt, {
   secret: env.JWT_SECRET,
